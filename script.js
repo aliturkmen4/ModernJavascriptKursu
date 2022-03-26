6a623a006c7b2746df745ac26fa2817fc65f15ee
Ali <alym11731996@gmail.com>
2022-03-26 23:24:48 +0300

    some basic definitions were studied

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Mmerhaba");
+alert("Merhaba");
+console.log(1234);
+console.log(true);
+console.log([1,2,3]);
+console.error("hata oluştu!"); //hata mesajı vermemize yarar!
+console.warn("bir uyarı oluştu!"); //uyarı mesajı vermemize yarar!
+
+//tek satırlık yorum satırı
+
+/*
+yorum satırları
+yorum satırları
+yorum satırları
+*/
+
+console.clear(); //console'u temizlemeye yarar!
+
+document.getElementById("header"); //id'si header olan elementi console ekranında bana getirir!
