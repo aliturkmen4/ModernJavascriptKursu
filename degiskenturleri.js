1ca59a2e583a829e2b005ef9fa5d2fe2d2d890b9
Ali <alym11731996@gmail.com>
2022-03-27 00:28:06 +0300

    introduction to variable types was happened

---
+++ degiskenturleri.js
@@ -0,0 +1,55 @@
+//Değişken Tipleri
+
+//1.Primitive Types
+
+//1.1 String
+
+let FirstName='Çınar';
+
+//1.2 Number
+
+let age2=26;
+
+let money=100.5;
+
+console.log(typeof age2); //tipini bilmediğim bir değişkenin tipine böyle bakabilirim!
+console.log(typeof money);
+
+//1.3 Boolean
+
+let isActive=true;
+
+console.log(typeof isActive);
+
+//1.4 Null
+
+let job=null;
+
+//1.5 Undefined
+
+let car;
+
+//2.Referance Types - Objects
+
+//2.1 Array
+
+let names=['ali','ahmet','can'];
+
+console.log(typeof names);
+
+//2.2 Objects
+
+let adress={
+    city:'Antalya',
+    country:'Turkiye'
+}
+
+console.log(typeof adress);
+
+//2.3 Function
+
+var calculateAge=function(){
+    return 0;
+};
+
+console.log(typeof calculateAge);
