a1769abfa4a6f1cb25536700ef46c12cccffc345
Ali <alym11731996@gmail.com>
2022-04-04 13:01:00 +0300

    comment prompt was edited

--- DOM/eventlisteners.js
+++ DOM/eventlisteners.js
@@ -8,19 +8,19 @@ btn.addEventListener('click',function(){ //hangi işlemi yapacağımı yazıyoru
     console.log('btn clicked!');
 });
 
-// btn.addEventListener('click',btnClick); //bu şekilde dışarıda hazırladağım fonksiyonu da parametre olarak yazabilirim!
+btn.addEventListener('click',btnClick); //bu şekilde dışarıda hazırladağım fonksiyonu da parametre olarak yazabilirim!
 
-// function btnClick(){
-//     console.log('btn clicked!')
-// }
+function btnClick(){
+    console.log('btn clicked!')
+}
 
-// btn2.addEventListener('click',btnClick); //burada btn ve btn2 olmak üzere iki ayrı nesne var ve aynı fonksiyon aracılığıyla click işlemimi gerçekleştirebiliyorum!
+btn2.addEventListener('click',btnClick); //burada btn ve btn2 olmak üzere iki ayrı nesne var ve aynı fonksiyon aracılığıyla click işlemimi gerçekleştirebiliyorum!
 
-// function btnClick2(){
-//     console.log('btn 2 clicked!')
-// }
+function btnClick2(){
+    console.log('btn 2 clicked!')
+}
 
-// btn.addEventListener('click',btnClick2); //bir buton için 2 click olayı atayabilirim!
+btn.addEventListener('click',btnClick2); //bir buton için 2 click olayı atayabilirim!
 
 //a etiketinin normalde görevi bir linke gitmek!
 
